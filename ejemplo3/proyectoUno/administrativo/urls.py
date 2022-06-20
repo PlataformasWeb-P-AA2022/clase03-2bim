3817c8dcfdd4179288eac56319ca3f48bb73bcae
@reroes <reroes799@gmail.com>
2022-06-20 15:39:58 -0500

    ejemplos para clases

---
+++ ejemplo3/proyectoUno/administrativo/urls.py
@@ -0,0 +1,14 @@
+"""
+    Manejo de urls para la aplicación
+    administrativo
+"""
+from django.urls import path
+# se importa las vistas de la aplicación
+from . import views
+
+
+urlpatterns = [
+        path('bienvenidos', views.index, name='index'),
+        path('estudiante/<int:id>', views.obtener_estudiante,
+            name='obtener_estudiante'),
+ ]
